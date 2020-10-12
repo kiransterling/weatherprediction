d08118348a44f66124fe403824a6a0fdbf8e25a5
Kashyap Kiran Das <KashyapKiran.Das@team.telstra.com>
2020-10-12 12:11:17 +0530

    changed server url

--- src/main/java/com/example/weatherprediction/config/Config.java
+++ src/main/java/com/example/weatherprediction/config/Config.java
@@ -4,13 +4,20 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.servers.Server;
+
 @Configuration
 public class Config {
-
 
 	@Bean
 	public RestTemplate getRestTemplate() {
 		return new RestTemplate();
+	}
+
+	@Bean
+	public OpenAPI openAPI() {
+		return new OpenAPI().addServersItem(new Server().url("/"));
 	}
 
 }
